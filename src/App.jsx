dddd8f5601aa02da939fdb8986334cf8111c4281
Nimesh Dulal <nimeshdulal870@gmail.com>
2026-06-03 11:32:27 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      App
+      <p className='text-3xl font-bold bg-green-500'>Hello, Tailwind</p>
+    </div>
+  )
+}
+
+export default App
